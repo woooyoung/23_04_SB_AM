f236cd9b5160ec624ee2cefe3fc43811d165469c
woooyoung <axdsw121@gmail.com>
2023-04-13 15:31:36 +0900

    count 변수의 값 초기화 및 증가

--- src/main/java/com/KoreaIT/cwy/demo/controller/UsrHomeController.java
+++ src/main/java/com/KoreaIT/cwy/demo/controller/UsrHomeController.java
@@ -6,9 +6,37 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UsrHomeController {
+
+	private int count;
+
+	public UsrHomeController() {
+		count = 0;
+	}
+
 	@RequestMapping("/usr/home/main")
 	@ResponseBody
 	public String showMain() {
 		return "안녕하세요";
+	}
+
+	@RequestMapping("/usr/home/main2")
+	@ResponseBody
+	public String showMain2() {
+		return "잘가";
+	}
+
+	@RequestMapping("/usr/home/main3")
+	@ResponseBody
+	public int showMain3() {
+		int a = 1;
+		int b = 2;
+		return a + b;
+	}
+
+	@RequestMapping("/usr/home/main4")
+	@ResponseBody
+	public int showMain4() {
+
+		return count++;
 	}
 }
