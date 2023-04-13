fcc2714edf4cb7f123a3f2a7fd941ceb5628096a
woooyoung <axdsw121@gmail.com>
2023-04-13 16:38:54 +0900

    액션메서드에서 각각의 리턴타입 실험

--- src/main/java/com/KoreaIT/cwy/demo/controller/UsrHomeController.java
+++ src/main/java/com/KoreaIT/cwy/demo/controller/UsrHomeController.java
@@ -1,29 +1,104 @@
 package com.KoreaIT.cwy.demo.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Controller
 public class UsrHomeController {
 
-	private int count;
-
-	public UsrHomeController() {
-		count = 0;
-	}
-
-	@RequestMapping("/usr/home/getCount")
+	@RequestMapping("/usr/home/getArticles")
 	@ResponseBody
-	public int getCount() {
-		return count++;
+	public List<Article> getArticles() {
+		Article article1 = new Article(1, "제목1");
+		Article article2 = new Article(2, "제목2");
+
+		List<Article> articles = new ArrayList<>();
+		articles.add(article1);
+		articles.add(article2);
+
+		return articles;
 	}
 
-	@RequestMapping("/usr/home/setCount")
+	@RequestMapping("/usr/home/getArticle")
 	@ResponseBody
-	public String setCount(int count) {
-		this.count = count;
-		return "count의 값이 " + this.count + "으로 초기화 됨";
+	public Article getArticle() {
+		Article article = new Article(1, "제목1");
 
+		return article;
 	}
+
+	@RequestMapping("/usr/home/getList")
+	@ResponseBody
+	public List<String> getList() {
+		List<String> list = new ArrayList<>();
+		list.add("철수나이");
+		list.add("영희나이");
+
+		return list;
+	}
+
+	@RequestMapping("/usr/home/getMap")
+	@ResponseBody
+	public Map<String, Object> getMap() {
+		Map<String, Object> map = new HashMap<>();
+		map.put("철수나이", 22);
+		map.put("영희나이", 3);
+
+		return map;
+	}
+
+	@RequestMapping("/usr/home/getChar")
+	@ResponseBody
+	public char getChar() {
+		return 'a';
+	}
+
+	@RequestMapping("/usr/home/getBoolean")
+	@ResponseBody
+	public boolean getBoolean() {
+		return true;
+	}
+
+	@RequestMapping("/usr/home/getDouble")
+	@ResponseBody
+	public double getDouble() {
+		return 10.5;
+	}
+
+	@RequestMapping("/usr/home/getInt")
+	@ResponseBody
+	public int getInt() {
+		return 10;
+	}
+
+	@RequestMapping("/usr/home/getString")
+	@ResponseBody
+	public String getString() {
+		return "안녕";
+	}
+
+	@RequestMapping("/usr/home/getFloat")
+	@ResponseBody
+	public float getFloat() {
+		return 10.5f;
+	}
+}
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class Article {
+	private int id;
+	private String title;
+
 }
