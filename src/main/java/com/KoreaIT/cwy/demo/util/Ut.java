246cddf77b0c87dfaf266fc3aad9caef5c7eda12
woooyoung <axdsw121@gmail.com>
2023-04-17 16:47:46 +0900

    Ut.empty 함수 도입

---
+++ src/main/java/com/KoreaIT/cwy/demo/util/Ut.java
@@ -0,0 +1,20 @@
+package com.KoreaIT.cwy.demo.util;
+
+public class Ut {
+
+	public static boolean empty(Object obj) {
+
+		if (obj == null) {
+			return true;
+		}
+
+		if (obj instanceof String == false) {
+			return true;
+		}
+
+		String str = (String) obj;
+
+		return str.trim().length() == 0;
+	}
+
+}
