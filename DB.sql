89c94077ca1f5c67111edfbc8755d8df20971732
woooyoung <axdsw121@gmail.com>
2023-04-17 14:54:48 +0900

    회원 테이블 추가

--- DB.sql
+++ DB.sql
@@ -31,6 +31,58 @@ updateDate = NOW(),
 title = '제목 3',
 `body` = '내용 3';
 
+# 회원 테이블 생성
+CREATE TABLE `member`(
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    loginId CHAR(20) NOT NULL,
+    loginPw CHAR(60) NOT NULL,
+    `authLevel` SMALLINT(2) UNSIGNED DEFAULT 3 COMMENT '권한 레벨 (3=일반,7=관리자)',
+    `name` CHAR(20) NOT NULL,
+    nickname CHAR(20) NOT NULL,
+    cellphoneNum CHAR(20) NOT NULL,
+    email CHAR(50) NOT NULL,
+    delStatus TINYINT(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '탈퇴 여부 (0=탈퇴 전, 1=탈퇴 후)',
+    delDate DATETIME COMMENT '탈퇴 날짜'
+);
+
+# 회원 테스트데이터 생성 (관리자)
+INSERT INTO `member` 
+SET regDate = NOW(),
+updateDate = NOW(),
+loginId = 'admin',
+loginPw = 'admin',
+`authLevel` = 7,
+`name` = '관리자',
+`nickname` = '관리자',
+cellphoneNum = '01012341234',
+email = 'abcdef@gmail.com';
+
+# 회원 테스트데이터 생성 (일반)
+INSERT INTO `member` 
+SET regDate = NOW(),
+updateDate = NOW(),
+loginId = 'test1',
+loginPw = 'test1',
+`name` = '회원1',
+`nickname` = '회원1',
+cellphoneNum = '01043214321',
+email = 'abcd@gmail.com';
+
+INSERT INTO `member` 
+SET regDate = NOW(),
+updateDate = NOW(),
+loginId = 'test2',
+loginPw = 'test2',
+`name` = '회원2',
+`nickname` = '회원2',
+cellphoneNum = '01067896789',
+email = 'zxcv@gmail.com';
+
 SELECT * FROM article;
+SELECT * FROM `member`;
+
+DESC `member`;
 
 SELECT LAST_INSERT_ID();
