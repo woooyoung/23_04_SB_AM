8c56fa446c3580de9738520811666057c5fc8156
woooyoung <axdsw121@gmail.com>
2023-04-18 15:46:53 +0900

    doModify, doDelete 에 ResultData 적용

--- src/main/java/com/KoreaIT/cwy/demo/controller/UsrArticleController.java
+++ src/main/java/com/KoreaIT/cwy/demo/controller/UsrArticleController.java
@@ -21,28 +21,28 @@ public class UsrArticleController {
 	// 액션메서드
 	@RequestMapping("/usr/article/doModify")
 	@ResponseBody
-	public Object doModify(int id, String title, String body) {
+	public ResultData<Integer> doModify(int id, String title, String body) {
 		Article article = articleService.getArticle(id);
 		if (article == null) {
-			return id + "번 글은 존재하지 않습니다";
+			return ResultData.from("F-1", Ut.f("%d번 글은 존재하지 않습니다", id), id);
 		}
 
 		articleService.modifyArticle(id, title, body);
 
-		return article;
+		return ResultData.from("S-1", Ut.f("%d번 글을 수정 했습니다", id), id);
 	}
 
 	@RequestMapping("/usr/article/doDelete")
 	@ResponseBody
-	public String doDelete(int id) {
+	public ResultData<Integer> doDelete(int id) {
 		Article article = articleService.getArticle(id);
 		if (article == null) {
-			return id + "번 글은 존재하지 않습니다";
+			return ResultData.from("F-1", Ut.f("%d번 글은 존재하지 않습니다", id), id);
 		}
 
 		articleService.deleteArticle(id);
 
-		return id + "번 글을 삭제했습니다";
+		return ResultData.from("S-1", Ut.f("%d번 글을 삭제 했습니다", id), id);
 	}
 
 	@RequestMapping("/usr/article/doWrite")
