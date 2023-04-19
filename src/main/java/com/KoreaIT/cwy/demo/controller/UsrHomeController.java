90cbb559737e7c7df502a760f91566697c4e2576
woooyoung <axdsw121@gmail.com>
2023-04-19 14:48:40 +0900

    JSP 연결

--- src/main/java/com/KoreaIT/cwy/demo/controller/UsrHomeController.java
+++ src/main/java/com/KoreaIT/cwy/demo/controller/UsrHomeController.java
@@ -2,16 +2,18 @@ package com.KoreaIT.cwy.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UsrHomeController {
 
 	@RequestMapping("/usr/home/main")
-	@ResponseBody
 	public String showMain() {
+		return "usr/home/main";
+	}
 
-		return "MAIN HI";
+	@RequestMapping("/")
+	public String showRoot() {
+		return "redirect:/usr/home/main";
 	}
 
 }
---
+++ src/main/webapp/WEB-INF/jsp/usr/home/main.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>메인 페이지</title>
+</head>
+<body>
+
+		<div>MAIN 123</div>
+
+</body>
+</html>
