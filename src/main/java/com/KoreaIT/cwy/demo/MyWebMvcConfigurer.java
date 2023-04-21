2d5936e52d170bb644f15e30c0a5caff1f8b77d4
woooyoung <axdsw121@gmail.com>
2023-04-21 14:24:35 +0900

    NeedLoginInterceptor 구현 및 MyWebMvcConfigurer 등록

--- src/main/java/com/KoreaIT/cwy/demo/MyWebMvcConfigurer.java
+++ src/main/java/com/KoreaIT/cwy/demo/MyWebMvcConfigurer.java
@@ -6,6 +6,7 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.KoreaIT.cwy.demo.interceptor.BeforeActionInterceptor;
+import com.KoreaIT.cwy.demo.interceptor.NeedLoginInterceptor;
 
 @Configuration
 public class MyWebMvcConfigurer implements WebMvcConfigurer {
@@ -13,11 +14,19 @@ public class MyWebMvcConfigurer implements WebMvcConfigurer {
 	@Autowired
 	BeforeActionInterceptor beforeActionInterceptor;
 
+	// NeedLoginInterceptor 불러오기
+	@Autowired
+	NeedLoginInterceptor needLoginInterceptor;
+
 	// /resource/common.css
 	// 인터셉터 적용
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(beforeActionInterceptor).addPathPatterns("/**").excludePathPatterns("/resource/**")
 				.excludePathPatterns("/error");
+
+		registry.addInterceptor(needLoginInterceptor).addPathPatterns("/usr/article/write")
+				.addPathPatterns("/usr/article/doWrite").addPathPatterns("/usr/article/modify")
+				.addPathPatterns("/usr/article/doModify").addPathPatterns("/usr/article/doDelete");
 	}
 
 }
---
+++ src/main/java/com/KoreaIT/cwy/demo/interceptor/NeedLoginInterceptor.java
@@ -0,0 +1,20 @@
+package com.KoreaIT.cwy.demo.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class NeedLoginInterceptor implements HandlerInterceptor {
+
+	@Override
+	public boolean preHandle(HttpServletRequest req, HttpServletResponse resp, Object handler) throws Exception {
+
+		System.out.println("로그인 하고 써라");
+
+		return HandlerInterceptor.super.preHandle(req, resp, handler);
+	}
+
+}
