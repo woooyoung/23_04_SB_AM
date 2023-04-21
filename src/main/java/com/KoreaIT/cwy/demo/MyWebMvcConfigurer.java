dcb50fc5b3c6e442d9b21166bf45bc72fdfe8199
woooyoung <axdsw121@gmail.com>
2023-04-21 13:51:51 +0900

    BeforeActionInterceptor 를 registry에 등록

---
+++ src/main/java/com/KoreaIT/cwy/demo/MyWebMvcConfigurer.java
@@ -0,0 +1,23 @@
+package com.KoreaIT.cwy.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.KoreaIT.cwy.demo.interceptor.BeforeActionInterceptor;
+
+@Configuration
+public class MyWebMvcConfigurer implements WebMvcConfigurer {
+	// BeforeActionInterceptor 불러오기
+	@Autowired
+	BeforeActionInterceptor beforeActionInterceptor;
+
+	// /resource/common.css
+	// 인터셉터 적용
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(beforeActionInterceptor).addPathPatterns("/**").excludePathPatterns("/resource/**")
+				.excludePathPatterns("/error");
+	}
+
+}
--- src/main/java/com/KoreaIT/cwy/demo/interceptor/BeforeActionInterceptor.java
+++ src/main/java/com/KoreaIT/cwy/demo/interceptor/BeforeActionInterceptor.java
@@ -11,7 +11,9 @@ public class BeforeActionInterceptor implements HandlerInterceptor {
 
 	@Override
 	public boolean preHandle(HttpServletRequest req, HttpServletResponse resp, Object handler) throws Exception {
-		System.out.println("============실행됨?==============");
+		
+		System.out.println("실행됨?");
+		
 		return HandlerInterceptor.super.preHandle(req, resp, handler);
 	}
 
