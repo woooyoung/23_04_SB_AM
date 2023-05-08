b4e03119c8b98faf1a7510e4588b9ca80effdf69
woooyoung <axdsw121@gmail.com>
2023-05-08 17:45:20 +0900

    Ajax 추가 설명

--- src/main/java/com/KoreaIT/cwy/demo/controller/UsrAjaxTestController.java
+++ src/main/java/com/KoreaIT/cwy/demo/controller/UsrAjaxTestController.java
@@ -1,5 +1,8 @@
 package com.KoreaIT.cwy.demo.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,7 +18,23 @@ public class UsrAjaxTestController {
 
 	@RequestMapping("usr/home/doPlus")
 	@ResponseBody
-	int doPlus(int num1, int num2) {
-		return num1 + num2;
+	String doPlus(int num1, int num2) {
+		String msg = "더하기 성공!";
+
+		int rs = num1 + num2;
+
+		return rs + "/" + msg + "/S-1";
+	}
+
+	@RequestMapping("usr/home/doPlusJson")
+	@ResponseBody
+	Map doPlusJson(int num1, int num2) {
+		Map rs = new HashMap<String, Object>();
+
+		rs.put("rs", num1 + num2);
+		rs.put("msg", "더하기 성공!");
+		rs.put("code", "S-1");
+
+		return rs;
 	}
 }
